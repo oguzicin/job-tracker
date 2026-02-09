e03331d0cb6e902cd9666d5033b6529a573decaa
oguzıcın <oguzcn@gmal.com>
2026-02-09 17:57:16 +0300

    db update

--- server/db.js
+++ server/db.js
@@ -2,17 +2,11 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = isProduction
-  ? process.env.DATABASE_URL
-  : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-
-console.log("DB_PASSWORD value:", process.env.DB_PASSWORD);
+// Canlıda sadece DATABASE_URL, lokalde manuel bilgiler kullanılır
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: isProduction 
+    ? process.env.DATABASE_URL 
+    : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
   ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
